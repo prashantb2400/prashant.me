fafea7d20e8b16dcdd206169381e00d362ca9879
Prashant Bhushan <tradexmeme@gmail.com>
2026-02-22 16:03:06 +0530

    fixed social media links

--- src/constants.ts
+++ src/constants.ts
@@ -4,37 +4,30 @@ export const SOCIALS = [
   {
     name: "Github",
     href: "https://github.com/prashantb2400",
-    linkTitle: ` ${SITE.title} on Github`,
+    linkTitle: `${SITE.title} on Github`,
     icon: "github",
-    active: false,
+    active: true,
   },
   {
     name: "X",
     href: "https://x.com/knightofslights",
     linkTitle: `${SITE.title} on X`,
     icon: "twitter",
-    active: false,
-  },
-  {
-    name: "BlueSky",
-    href: "active: false",
-    linkTitle: `${SITE.title} on BlueSky`,
-    icon: "bluesky",
-    active: false,
+    active: true,
   },
   {
     name: "LinkedIn",
     href: "https://www.linkedin.com/in/prashant-bhushan-3815a535a/",
     linkTitle: `${SITE.title} on LinkedIn`,
     icon: "linkedin",
-    active: false,
+    active: true,
   },
   {
     name: "Mail",
     href: "mailto:prashantbhushan24@gmail.com",
     linkTitle: `Send an email to ${SITE.title}`,
     icon: "mail",
-    active: false,
+    active: true,
   },
 ] as const;
 
@@ -44,12 +37,6 @@ export const SHARE_LINKS = [
     href: "https://x.com/intent/post?url=",
     linkTitle: `Share this post on X`,
     icon: "twitter",
-  },
-  {
-    name: "BlueSky",
-    href: "https://bsky.app/intent/compose?text=",
-    linkTitle: `Share this post on BlueSky`,
-    icon: "bluesky",
   },
   {
     name: "LinkedIn",
@@ -62,24 +49,6 @@ export const SHARE_LINKS = [
     href: "https://wa.me/?text=",
     linkTitle: `Share this post via WhatsApp`,
     icon: "whatsapp",
-  },
-  {
-    name: "Facebook",
-    href: "https://www.facebook.com/sharer.php?u=",
-    linkTitle: `Share this post on Facebook`,
-    icon: "facebook",
-  },
-  {
-    name: "Telegram",
-    href: "https://t.me/share/url?url=",
-    linkTitle: `Share this post via Telegram`,
-    icon: "telegram",
-  },
-  {
-    name: "Pinterest",
-    href: "https://pinterest.com/pin/create/button/?url=",
-    linkTitle: `Share this post on Pinterest`,
-    icon: "pinterest",
   },
   {
     name: "Mail",
